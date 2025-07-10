b1ff7dd6474629e899d1f5855894d03d4efe2ca7
Maik <14537856+maikgr@users.noreply.github.com>
2025-07-10 16:42:29 +0800

    Add deploy config for cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  },
   devtools: {
     enabled: true,
-
     timeline: {
       enabled: true
     }
