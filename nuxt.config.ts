7df9b7264d0b31b126a5d463b341ec0536cf91ac
Maik <14537856+maikgr@users.noreply.github.com>
2025-07-14 18:36:43 +0800

    update site icon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,7 +60,7 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: 'D&D Journal',
+      title: 'Lyrial\'s Corposcape Journal',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
